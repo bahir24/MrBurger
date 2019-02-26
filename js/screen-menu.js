bf497609228e7b3846ba5673d04c42ab07b6283d
bahir24 <7074033@gmail.com>
2019-02-26 22:14:16 +0300

    week3

---
+++ js/screen-menu.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector('.screen-menu');
+var hamburger = document.querySelector('.hamburger-menu-link');
+var close = document.querySelector('.screen-menu__close');
+hamburger.addEventListener ('click', function(event) {
+  event.preventDefault();
+  menu.classList.add('menu__active');
+});
+close.addEventListener ('click', function(event) {
+  event.preventDefault();
+  menu.classList.remove('menu__active');
+});
