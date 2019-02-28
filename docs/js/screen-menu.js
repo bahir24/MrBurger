c42bc833aea7729d3663ead3197e6f61f22b75ae
Mikhail <44851024+bahir24@users.noreply.github.com>
2019-02-28 17:42:44 +0300

    Update screen-menu.js

--- docs/js/screen-menu.js
+++ docs/js/screen-menu.js
@@ -4,8 +4,10 @@ var close = document.querySelector('.screen-menu__close');
 hamburger.addEventListener ('click', function(event) {
   event.preventDefault();
   menu.classList.add('menu__active');
+  document.body.style.overflow = "hidden";
 });
 close.addEventListener ('click', function(event) {
   event.preventDefault();
   menu.classList.remove('menu__active');
+  document.body.style.overflow = "auto";
 });
